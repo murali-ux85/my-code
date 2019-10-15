4617e97880df92a46a961fbf181876a24d884ef4
Murali Krishna <muralipan85@gmail.com>
2019-10-15 08:41:52 +0000

    Added new helloworld javafile, first commit

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+//Murali This is a Helloworld java program, second addition
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
